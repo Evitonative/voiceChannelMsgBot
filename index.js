3e45f94569b1cf89a465e2950209d2ce50cca678
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-04-14 13:21:49 +0200

    More Messages
    Added multi message support and Logging

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ const client = new Discord.Client();
 const fs = require('fs');
 
 let config = JSON.parse(fs.readFileSync('config.json'));
+let configuredMessages = 0;
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('[INFO] Bot online');
     client.user.setStatus("invisible");
+    console.log('[INFO] Bot set status to invisible');
+    let Counting = true;
+    console.log('[INFO] Counting configured Messages')
+    while (Counting){
+        if(config.hasOwnProperty('message' + (configuredMessages + 1))){
+            configuredMessages++;
+            let msg = '<@&'+ config['message' + configuredMessages].roleToMention +'> <@123> ' + config['message' + configuredMessages].msg
+            console.log('   > Message ' + configuredMessages + ': ' + msg);
+        }
+        else{
+            Counting = false;
+            console.log('[INFO] Found ' + configuredMessages + ' Configured Messages')
+        }
+    }
 })
 
-client.on('voiceStateUpdate', (oldState, newState) =>{
-    if (newState.guild != config.guild) return;
-    if (newState.channelID === config.messages1.vc) {
-        let msg = '<@&'+config.messages1.roleToMention+'> <@' + newState.member.id + '> ' + config.messages1.msg
-        client.channels.cache.get(config.messages1.msgChannel).send(msg);
-        console.log('msg > ', msg)
+client.on('voiceStateUpdate', (oldState, newState) =>{    
+    for(i = 1; i <= configuredMessages; i++){
+        if (newState.channelID === config['message' + i].vc && newState.guild == config['message' + i].guild) {
+            let msg = '<@&'+config['message' + i].roleToMention+'> <@' + newState.member.id + '> ' + config['message' + i].msg
+            client.channels.cache.get(config['message' + i].msgChannel).send(msg);
+            console.log('[INFO] Send message in ' + config['message' + i].guild + ' to' + config['message' + i].msgChannel + ' > ', msg)
+        }
     }
 })
 
